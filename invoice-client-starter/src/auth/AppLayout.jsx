bb1a64603f8adcad51f837a28eb458f8645f7c7a
Roman Przeczek <93882668+RomanPrzeczek@users.noreply.github.com>
2025-07-14 14:37:25 +0200

    FE layout improoved

--- invoice-client-starter/src/auth/AppLayout.jsx
+++ invoice-client-starter/src/auth/AppLayout.jsx
@@ -25,7 +25,7 @@ const AppLayout = () => {
   },[isLoggedIn]);
 
   return (
-    <div className="container">
+    <div className="container mt-4 mb-4">
       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row gap-3">
           <li className="nav-item">
