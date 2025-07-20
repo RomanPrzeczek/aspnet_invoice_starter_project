44c2f65b9a0908644de969b4fa11b5ca902a16e9
Roman Przeczek <93882668+RomanPrzeczek@users.noreply.github.com>
2025-07-20 11:40:01 +0200

    api.js updated with another clog for its test

--- invoice-client-starter/src/utils/api.js
+++ invoice-client-starter/src/utils/api.js
@@ -2,6 +2,7 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const fetchData = (url, requestOptions) => {
     const apiUrl = `${API_URL}${url}`;
+    console.log("Test of console log.");
     console.log("\n 🌐 VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
 
     return fetch(apiUrl, requestOptions)
