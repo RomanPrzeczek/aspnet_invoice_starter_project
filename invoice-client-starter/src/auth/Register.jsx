0217acb003d22cbcee5d33c201093e5ab7e1ef47
RomanPrzeczek <roman.przeczek1977@gmail.com>
2025-07-24 15:05:34 +0200

    Country enum ini-value corrected.

--- invoice-client-starter/src/auth/Register.jsx
+++ invoice-client-starter/src/auth/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
         zip: "",
         city: "",
         note: "",
-        country: "CzechRepublic", // výchozí hodnota enumu (přizpůsob pokud jiná)
+        country: "CZECHIA"
     });
 
     const handleChange = (e) => {
