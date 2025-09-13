5b0db57d77c6ef8b3a27f32c558ef87b148467ad
Roman Przeczek <93882668+RomanPrzeczek@users.noreply.github.com>
2025-09-13 20:47:26 +0200

    FE vite.config adjusted

--- invoice-client-starter/vite.config.js
+++ invoice-client-starter/vite.config.js
@@ -1,42 +1,46 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-// volitelně je odolnější používat path.resolve
-// import path from 'path'
+import fs from 'node:fs' 
 
-export default defineConfig(({ mode }) => {
-  const common = {
-    plugins: [react()],
-    server: {
-      host: 'fe.local.test',
-      port: 5173,
-      https: {
-        cert: fs.readFileSync('./certs/fe.local.test.pem'),
-        key:  fs.readFileSync('./certs/fe.local.test-key.pem'),
-        // cert: fs.readFileSync(path.resolve(__dirname, 'certs/fe.local.test.pem')),
-        // key:  fs.readFileSync(path.resolve(__dirname, 'certs/fe.local.test-key.pem')),
-      },
-    },
-    preview: {
-      host: 'fe.local.test',
-      port: 4173, // můžeš změnit
-      https: {
-        cert: fs.readFileSync('./certs/fe.local.test.pem'),
-        key:  fs.readFileSync('./certs/fe.local.test-key.pem'),
-      },
-    },
-  }
+export default defineConfig(({ command, mode }) => {
+  const cfg = { plugins: [react()] }
 
-  // Proxy jen v DEV (rychlé ladění); v prodsim bez proxy (1:1).
-  if (mode === 'development') {
-    common.server.proxy = {
-      '/api': {
-        target: 'https://api.local.test:5001',
-        changeOrigin: true,
-        secure: false, // pokud nastavíš NODE_EXTRA_CA_CERTS na mkcert CA, může být true
+  if (command === 'serve') {
+    const isProdSim = mode === 'prodsim'
+
+    if (isProdSim) {
+      cfg.server = {
+        host: 'fe.local.test',
+        port: 5173,
+        https: {
+          cert: fs.readFileSync('./certs/fe.local.test.pem'),
+          key:  fs.readFileSync('./certs/fe.local.test-key.pem'),
+        },
+      }
+    } else {
+      cfg.server = {
+        host: 'fe.local.test',
+        port: 5173,
+        https: true,
+        proxy: {
+          '/api': {
+            target: 'https://api.local.test:5001',
+            changeOrigin: true,
+            secure: false,
+          }
+        }
       }
     }
+  } else if (command === 'preview') {
+    cfg.preview = {
+      host: 'fe.local.test',
+      port: 4173,
+      https: {
+        cert: fs.readFileSync('./certs/fe.local.test.pem'),
+        key:  fs.readFileSync('./certs/fe.local.test-key.pem'),
+      },
+    }
   }
-
-  return common
+  return cfg
 })
